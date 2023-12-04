ff29be122fb460eb8a8ae070ae9bd3d479e8994c
ibraam <ibraam.nashaat2@gmail.com>
2023-12-04 12:53:07 +0200

    [FR] Updated SignIn stress test

--- stress-testing/scripts/SignIn.js
+++ stress-testing/scripts/SignIn.js
@@ -1,16 +1,35 @@
-import { check } from 'k6';
+import { check, sleep } from 'k6';
 
 import http from 'k6/http';
 import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
 
 export let options = {
-    vus: 2,
-    duration: '30s',
+    thresholds: {
+        http_req_duration: [{ threshold: 'p(95) < 20000', abortOnFail: true }],
+    },
+    stages: [
+        {
+            duration: '10s',
+            target: 100,
+        },
+        {
+            duration: '50s',
+            target: 700,
+        },
+        {
+            duration: '10s',
+            target: 700,
+        },
+        {
+            duration: '50s',
+            target: 0,
+        },
+    ],
 };
 
 export default function () {
     let payload = JSON.stringify({
-        UUID: 'Claudie@gmail.com',
+        UUID: 'Art@gmail.com',
         password: '12345678tT@',
     });
 
@@ -19,7 +38,7 @@ export default function () {
     };
 
     let response = http.post(
-        'http://16.171.65.142:3000/api/v1/auth/login',
+        'http://localhost:3000/api/v1/auth/login',
         payload,
         {
             headers: headers,
@@ -29,6 +48,7 @@ export default function () {
     check(response, {
         'is status 200': (r) => r.status === 200,
     });
+    sleep(1);
 }
 
 export function handleSummary(data) {
