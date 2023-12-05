3ca0397c9c729a235c5b9a7addb3b628af612296
Michael Ehab <michaelehab16@gmail.com>
2023-12-05 20:42:24 +0200

    Add follow and unfollow stress test

---
+++ stress-testing/scripts/FollowUnfollow.js
@@ -0,0 +1,31 @@
+import { check, sleep } from "k6";
+
+import http from "k6/http";
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { SharedOptions } from "./Shared.js";
+import { getUniqueEmail, getAuthenticatedRequestHeaders } from "./utils.js";
+
+export let options = SharedOptions;
+
+export default function () {
+  let username = getUniqueEmail();
+  let headers = getAuthenticatedRequestHeaders();
+
+  let response = http.post(
+    `http://localhost:3001/api/v1/users/follow/${username}`,
+    {
+      headers: headers,
+    }
+  );
+
+  check(response, {
+    "is status 200": (r) => r.status === 200,
+  });
+  sleep(1);
+}
+
+export function handleSummary(data) {
+  return {
+    "../reports/checkUsernameUniquenessReport.html": htmlReport(data),
+  };
+}
