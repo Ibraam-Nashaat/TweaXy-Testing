a88b78a0d83f1edd11186b882674d85462ba68cd
ibraam <ibraam.nashaat2@gmail.com>
2023-12-05 21:19:48 +0200

    [FR] Fixed utils.js

--- stress-testing/scripts/utils.js
+++ stress-testing/scripts/utils.js
@@ -2,43 +2,49 @@ import http from 'k6/http';
 import { TestUser } from './Shared.js';
 
 class TokenSingleton {
-  constructor() {
-    this.token = null;
-  }
-
-  async getToken() {
-    if (!this.token) {
-      let payload = JSON.stringify(TestUser[0]);
-      let headers = {
-        "Content-Type": "application/json",
-      };
-
-  let response = http.post("http://localhost:3000/api/v1/auth/login", payload, {
-    headers: headers,
-  });
-
-  let token = response.data.token;
-
-    return {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + token,
-    };
-};
-
-const generateRandomString = (length) => {
-    let chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
-    let str = '';
-    for (let i = 0; i < length; i++) {
-        let index = Math.floor(Math.random() * chars.length);
-        let char = chars[index];
-        str += char;
+    constructor() {
+        this.token = null;
     }
-    return str;
-};
 
-const getUniqueEmail = () => {
-  let username = generateRandomString(8);
-  let domain = generateRandomString(5);
-  let email = username + "@" + domain + ".com";
-  return email;
-};
+    async getToken() {
+        if (!this.token) {
+            let payload = JSON.stringify(TestUser[0]);
+            let headers = {
+                'Content-Type': 'application/json',
+            };
+
+            let response = http.post(
+                'http://localhost:3000/api/v1/auth/login',
+                payload,
+                {
+                    headers: headers,
+                }
+            );
+
+            let token = response.data.token;
+
+            return {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + token,
+            };
+        }
+
+        const generateRandomString = (length) => {
+            let chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+            let str = '';
+            for (let i = 0; i < length; i++) {
+                let index = Math.floor(Math.random() * chars.length);
+                let char = chars[index];
+                str += char;
+            }
+            return str;
+        };
+
+        const getUniqueEmail = () => {
+            let username = generateRandomString(8);
+            let domain = generateRandomString(5);
+            let email = username + '@' + domain + '.com';
+            return email;
+        };
+    }
+}
