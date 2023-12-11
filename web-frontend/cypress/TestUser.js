ddae03eb9f594f15cea572fe444dadd06f480bca
ibraam <ibraam.nashaat2@gmail.com>
2023-12-11 23:09:22 +0200

    [FR] Added update password tests and updated tweets tests for frontend

--- web-frontend/cypress/TestUser.js
+++ web-frontend/cypress/TestUser.js
@@ -1,5 +1,22 @@
-const TestUser = {
-    EMAIL: "1cea3898-f19c-4515-90e4-1f8a50f87666@email.webhook.site",
-    PASSWORD: "aAbBcC$123"
-};
+const TestUser = [
+    {
+        EMAIL: 'b605f73c-bacf-4f37-a700-28224533267d@email.webhook.site',
+        PASSWORD: 'Ahmed@1234',
+    },
+    {
+        EMAIL: 'jocktmp+njziw@gmail.com',
+        PASSWORD: '12345678tT@',
+    },
+    {
+        EMAIL: 'combtmp+c4pq5@gmail.com',
+        PASSWORD: 'Levi@1234',
+        USERNAME: 'Levi',
+        NEW_PASSWORD: 'Levi@12345',
+    },
+    {
+        EMAIL: 'li.agg.ei.2.9o@gmail.com',
+        PASSWORD: 'Mikasa@1234',
+        USERNAME: 'Mikasa',
+    },
+];
 export default TestUser;
--- web-frontend/cypress/e2e/Tweet.cy.js
+++ web-frontend/cypress/e2e/Tweet.cy.js
@@ -3,8 +3,12 @@ import MainPageSelectors from '../../src/shared/selectors/MainPage.js';
 import TestUser from '../TestUser.js';
 import HomePageSelectors from '../../src/shared/selectors/HomePage.js';
 import ProfilePageSelectors from '../../src/shared/selectors/ProfilePage.js';
+import { hashText } from '../../src/shared/Utils.js';
+import TweetSelectors from '../../src/shared/selectors/Tweets.js';
 
 describe('Tweets Tests:', () => {
+    let post = Math.random();
+
     it('Add Tweet successfully', () => {
         cy.visit(`/`);
         cy.get(`[data-test="${MainPageSelectors.SIGN_IN}"]`).click();
@@ -18,14 +22,10 @@ describe('Tweets Tests:', () => {
         );
         cy.get(`[data-test="${SignInSelectors.NEXT_BUTTON}"]`).click();
 
-        const post = Math.random();
         cy.get(`[data-test="${HomePageSelectors.TWEETBOX_FIELD}"]`).type(post);
         cy.get(
             `[data-test="${HomePageSelectors.TWEETBOX_POST_BUTTON}"]`
         ).click();
-        cy.wait(3000);
-        cy.reload();
-        cy.wait(2000);
         cy.location('pathname').should('eq', '/home');
         cy.document().contains(post);
 
@@ -50,5 +50,73 @@ describe('Tweets Tests:', () => {
 
         cy.document().contains(post);
         cy.document().contains(TestUser[2].USERNAME);
+    });
+
+    it('Trying to delete post not posted by the same user', () => {
+        cy.visit(`/`);
+        cy.get(`[data-test="${MainPageSelectors.SIGN_IN}"]`).click();
+        cy.get(`[data-test="${SignInSelectors.EMAIL_FIELD}"]`).type(
+            `${TestUser[3].EMAIL}`
+        );
+        cy.wait(1000);
+        cy.get(`[data-test="${SignInSelectors.NEXT_BUTTON}"]`).click();
+        cy.get(`[data-test="${SignInSelectors.PASSWORD_FIELD}"]`).type(
+            `${TestUser[3].PASSWORD}`
+        );
+        cy.get(`[data-test="${SignInSelectors.NEXT_BUTTON}"]`).click();
+
+        cy.get(
+            `[data-test="${hashText(
+                TweetSelectors.TWEET_OPTIONS + TestUser[2].USERNAME + post
+            )}"]`
+        ).click();
+        cy.document().its('body.textContent').should('not.include', 'Delete');
+    });
+
+    it('Delete previously added tweet', () => {
+        cy.visit(`/`);
+        cy.get(`[data-test="${MainPageSelectors.SIGN_IN}"]`).click();
+        cy.get(`[data-test="${SignInSelectors.EMAIL_FIELD}"]`).type(
+            `${TestUser[2].EMAIL}`
+        );
+        cy.wait(1000);
+        cy.get(`[data-test="${SignInSelectors.NEXT_BUTTON}"]`).click();
+        cy.get(`[data-test="${SignInSelectors.PASSWORD_FIELD}"]`).type(
+            `${TestUser[2].PASSWORD}`
+        );
+        cy.get(`[data-test="${SignInSelectors.NEXT_BUTTON}"]`).click();
+
+        cy.get(
+            `[data-test="${hashText(
+                TweetSelectors.TWEET_OPTIONS + TestUser[2].USERNAME + post
+            )}"]`
+        ).click();
+
+        cy.get(`[data-test="${TweetSelectors.DELETE_TWEET}"]`).click();
+
+        cy.get(`[data-test="${TweetSelectors.CONFIRM_DELETE_TWEET}"]`).click();
+
+        cy.document().its('body.textContent').should('not.include', post);
+
+        cy.get(`[data-test="${HomePageSelectors.PROFILE_BUTTON}"]`).click();
+        cy.wait(1000);
+        cy.document().its('body.textContent').should('not.include', post);
+
+        cy.get(`[data-test="${HomePageSelectors.ACCOUNT_BUTTON}"]`).click();
+        cy.get(`[data-test="${HomePageSelectors.LOGOUT_BUTTON}"]`).click();
+
+        // Sign in with another account to check if post is reflected on followers feed
+        cy.get(`[data-test="${MainPageSelectors.SIGN_IN}"]`).click();
+        cy.get(`[data-test="${SignInSelectors.EMAIL_FIELD}"]`).type(
+            `${TestUser[3].EMAIL}`
+        );
+        cy.wait(1000);
+        cy.get(`[data-test="${SignInSelectors.NEXT_BUTTON}"]`).click();
+        cy.get(`[data-test="${SignInSelectors.PASSWORD_FIELD}"]`).type(
+            `${TestUser[3].PASSWORD}`
+        );
+        cy.get(`[data-test="${SignInSelectors.NEXT_BUTTON}"]`).click();
+        cy.wait(1000);
+        cy.document().its('body.textContent').should('not.include', post);
     });
 });
