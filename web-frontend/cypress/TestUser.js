dd2ef2007e645d3a1d86c4ebc332599dc136b41b
Michael Ehab <michaelehab16@gmail.com>
2023-12-11 22:05:32 +0200

    [🎨 REF] Update TestUsers

--- web-frontend/cypress/TestUser.js
+++ web-frontend/cypress/TestUser.js
@@ -1,5 +1,17 @@
-const TestUser = {
-    EMAIL: "1cea3898-f19c-4515-90e4-1f8a50f87666@email.webhook.site",
+const TestUsers = [{
+    USERNAME: "b1ddb3418a234508bfa2d46f00d7f804_38940803",
+    EMAIL: "b1ddb341-8a23-4508-bfa2-d46f00d7f804@email.webhook.site",
     PASSWORD: "aAbBcC$123"
-};
-export default TestUser;
+},
+{
+    USERNAME: "bfcc77410bec4576bd01f4212c221b2b_03660262",
+    EMAIL: "bfcc7741-0bec-4576-bd01-f4212c221b2b@email.webhook.site",
+    PASSWORD: "aAbBcC$123"
+},
+{
+    USERNAME: "Levi12",
+    EMAIL: "combtmp+c4pq5@gmail.com",
+    PASSWORD: "Levi@1234"
+}
+];
+export default TestUsers;
