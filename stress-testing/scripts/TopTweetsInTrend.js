adbcd88f066fad74f7dc91526e98308c21fdaed2
Michael Ehab <michael.ehab@hotmail.com>
2023-12-27 23:56:55 +0200

    Add Top Tweets in Trend Stress Test Script

---
+++ stress-testing/scripts/TopTweetsInTrend.js
@@ -0,0 +1,31 @@
+import { check, sleep } from "k6";
+import http, { get } from "k6/http";
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { SharedOptions, TestUser } from "./Shared.js";
+import { getAuthenticatedRequestHeaders } from "./utils.js";
+
+export let options = SharedOptions;
+let header = null;
+export const setup = async () => {
+  header = await getAuthenticatedRequestHeaders();
+  return header;
+};
+export default function (header) {
+  const trend = "AvengersEndGame";
+  let response = http.get(
+    `http://localhost:3000/api/v1/trends/${trend}?limit=10&offset=0`,
+    {
+      headers: header,
+    }
+  );
+  check(response, {
+    "is status 200": (r) => r.status === 200,
+  });
+  sleep(1);
+}
+
+export function handleSummary(data) {
+  return {
+    "../reports/TopTweetsInTrend.html": htmlReport(data),
+  };
+}
